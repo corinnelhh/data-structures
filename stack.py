b3b4554c4911b54b67e489a6cb1660b33799d701
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-05 17:09:36 -0700

    first commit

---
+++ stack.py
@@ -0,0 +1,32 @@
+def Node(object):
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+    def get_data(self):
+        return self.data
+
+    def set_next(self, next_node):
+        self.next = next_node
+
+    def get_next(self):
+        return self.next
+
+
+def Stack(object):
+    def __init__(self, *data):
+        self.head = None
+        for val in data:
+            self.push(val)
+
+    def push(self, data):
+        a = Node(data, self.head)
+        self.head = a
+
+    def pop(self):
+        if self.head is None:
+            raise IndexError
+        our_returned_value = self.head.get_data()
+        self.head.set_next(None)
+        self.head = self.head.get_next()
+        return our_returned_value
