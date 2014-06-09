7b92c35ea85f73d0b54d266c28ce5759591c4340
drytuna <drytuna@gmail.com>
2014-06-09 10:54:58 -0500

    fixed Node

--- linked_list.py
+++ linked_list.py
@@ -3,15 +3,6 @@ class Node(object):
         self.data = data
         self.next = node
 
-    def set_next(self, node = None):
-        self.next = node
-
-    def get_next(self):
-        return self.next
-
-    def get_data(self):
-        return self.data
-
 class LinkedList(object):
     def __init__(self, *data):
         self.size = 0
@@ -25,7 +16,7 @@ class LinkedList(object):
         current_node = self.head
         while current_node:
             yield current_node
-            current_node = current_node.get_next()
+            current_node = current_node.next
 
     def insert(self, val):
         new_node = Node(val, self.head)
@@ -36,49 +27,49 @@ class LinkedList(object):
         old_node = self.head
         if old_node is None:
             raise IndexError
-        self.head = old_node.get_next()
+        self.head = old_node.next
         self.size -= 1
-        old_node.set_next(None)
-        return old_node.get_data()
+        old_node.next = None
+        return old_node.data
 
     def search(self, val):
         node = self.head
         while node is not None:
-            if node.get_data() == val:
+            if node.data == val:
                 return node
-            node = node.get_next()
+            node = node.next
         return None
 
     def remove(self, val):
         first = self.head
         # removing the head of the linked list
-        if first.get_data() == val:
+        if first.data == val:
             self.pop()
             return first
         # if first node is wrong, look for the right one and remove
-        while first.get_next() is not None:
-            if first.get_next().get_data() == val:
-                remove_node = first.get_next()
-                second = remove_node.get_next()
-                remove_node.set_next(None)
-                first.set_next(second)
+        while first.next is not None:
+            if first.next.data == val:
+                remove_node = first.next
+                second = remove_node.next
+                remove_node.next = None
+                first.next = second
                 self.size -= 1
                 return remove_node
-            first = first.get_next()
-        if first.get_next() is None:
+            first = first.next
+        if first.next is None:
             raise IndexError
 
     def toString(self):
         node = self.head
         result = "("
-        while node.get_next() is not None:
-            val = node.get_data()
+        while node.next is not None:
+            val = node.data
             if type(val) == type(u'hello'):
                 result = result + "'" + str(val) + "', "
             else:
                 result = result + "" + str(val) + ", "
-            node = node.get_next()
-        if type(node.get_data()) == type(u'hello'):
-            return result + "'" + str(node.get_data()) + "')"
+            node = node.next
+        if type(node.data) == type(u'hello'):
+            return result + "'" + str(node.data) + "')"
         else:
-            return result + "" + str(node.get_data()) + ")"
+            return result + "" + str(node.data) + ")"
--- test_linked_list.py
+++ test_linked_list.py
@@ -7,38 +7,19 @@ def test_node_constructor():
     assert a.data == u'a'
     assert a.next is None
 
-def test_node_set_next():
-    a = Node(u'a')
-    b = Node(u'b')
-    a.set_next(b)
-    assert a.next is b
-    assert a.next.data == b.data
-
-def test_node_get_next():
-    a = Node(u'a')
-    b = Node(u'b', a)
-    assert a.get_next() is None
-    assert b.get_next() is a
-
-def test_node_get_data():
-    a = Node(u'a')
-    b = Node(u'b', a)
-    assert a.get_data() == u'a'
-    assert b.get_data() == u'b'
-
 def test_list_constructor():
     a = LinkedList()
     assert a.size == 0
     assert a.head == None
     a = LinkedList(*[1,2,3,4,5])
     assert a.size == 5
-    assert a.head.get_data() == 5
+    assert a.head.data == 5
 
 def test_list_iterable():
     a = LinkedList(1,2,3,4,5)
     b = [1,2,3,4,5]
     for x in a:
-        assert b[x.get_data()-1] == x.get_data()
+        assert b[x.data-1] == x.data
 
 def test_list_insert():
     llist = LinkedList()
@@ -48,8 +29,8 @@ def test_list_insert():
     llist.insert(u'b')
     llist.insert(u'a')
     assert llist.size == 5
-    assert llist.head.get_data() == u'a'
-    assert llist.head.get_next().get_data() == u'b'
+    assert llist.head.data == u'a'
+    assert llist.head.next.data == u'b'
 
 def test_list_pop():
     llist = LinkedList(*[1,2,3,4,5])
@@ -57,21 +38,21 @@ def test_list_pop():
     assert llist.size == 4
     llist.pop()     # pop 4
     llist.pop()     # pop 3
-    assert llist.head.get_data() == 2
+    assert llist.head.data == 2
     assert llist.size == 2
 
 def test_list_search():
     llist = LinkedList(*[1,2,3,4,5])
     assert llist.search(6) is None
-    assert llist.search(5).get_next().get_data() == 4
-    assert llist.search(1).get_next() is None
+    assert llist.search(5).next.data == 4
+    assert llist.search(1).next is None
 
 def test_list_remove():
     llist = LinkedList(*[1,2,3,4,5])
-    assert llist.remove(3).get_next() is None
-    assert llist.search(4).get_next() is llist.search(2)
+    assert llist.remove(3).next is None
+    assert llist.search(4).next is llist.search(2)
     llist.remove(1)
-    assert llist.search(2).get_next() is None
+    assert llist.search(2).next is None
     assert llist.size == 3
 
 def test_list_toString():
