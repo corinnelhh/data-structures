e6b6d35d0b2c434c735d1a18bba9ac3a320b9682
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-11 19:05:02 -0700

    fixing my mess

--- test_queue.py
+++ test_queue.py
@@ -2,37 +2,42 @@ import pytest
 from queue import Node, Queue
 
 
-def test_Node():
-    a = Node(5)
-    b = Node('x')
-    assert a.data == 5
-    assert a.next is None
-    assert b is not a
-    assert b.data == 'x'
+def test_node():
+    our_node = Node("first")
+    assert our_node.data == "first"
+    assert our_node.next is None
+
+    our_next_node = Node("second", our_node)
+    assert our_next_node.data == "second"
+    assert our_next_node.next is our_node
 
 
-def test_queue_init():
-    q = Queue(1, 'a', 2, 'b', 3)
-    assert q.head.data == 1
-    assert q.tail.data == 3
-    assert q.size() == 5
-    assert q.tail.next is None
-    assert q.head.next.data == 'a'
+def test_queue_creation():
+    our_queue = Queue()
+    assert our_queue.head is None
+    assert our_queue.tail is None
+    assert our_queue.size == 0
 
 
 def test_enqueue():
-    q = Queue()
-    q.enqueue('a')
-    assert q.tail is q.head  # testing for a Queue of 1 element
-    q.dequeue()
-    assert q.size() == 0
-    q.enqueue('b')
-    assert q.size() == 1
-    assert q.head.data == 'b'
-    q.enqueue('c')
-    assert q.size() == 2
-    q.enqueue('d')
-    assert q.tail.data == 'd'
+    our_queue = Queue()
+    our_queue.enqueue("first")
+
+    assert our_queue.head.data == "first"
+    assert our_queue.tail.data == "first"
+    assert our_queue.size == 1
+
+    our_queue.enqueue("second")
+    our_queue.enqueue("third")
+
+    assert our_queue.head.data == "third"
+    assert our_queue.tail.data == "first"
+    assert our_queue.size == 3
+
+    assert our_queue.head.next.data == "second"
+    assert our_queue.tail.last.data == "second"
+    assert our_queue.head.next.last.data == "third"
+    assert our_queue.tail.next is None
 
 
 def test_dequeue():
