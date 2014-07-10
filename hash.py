370a700860f83340dfe0b3a868a17cb7633e8640
corinnelhh <corinne.hutchinson@gmail.com>
2014-07-10 13:48:18 -0700

    added bitmask to hash

--- hash.py
+++ hash.py
@@ -2,14 +2,28 @@
 
 class HashTable(object):
 
-    def __init__(self):
-        self.hashed = [0 for i in range(128)]
+    def __init__(self, n=128):
+        self.hashed = [[] for i in range(n)]
+        self.mask_size = n - 1
 
     def hash(self, item):
-        pass
+        if isinstance(item, basestring):
+            hashed_item = []
+            for el in item:
+                hashed_item.append(ord(el))
+            total = sum(hashed_item)
+        else:
+            raise TypeError
+        return self.mask_size & total
 
     def get(self, item):
         pass
 
     def set(self, item):
-        pass
+        key = self.hash(item)
+        self.hashed.insert([key], item)
+
+
+if __name__ == "__main__":
+    our_hash = HashTable()
+    print our_hash.hash("happydaysarehereagain")
