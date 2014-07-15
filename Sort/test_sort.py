8cfa0ef06efa835f07fa49b3ec1a0e29a894179f
drytuna <drytuna@gmail.com>
2014-07-15 16:27:11 -0700

    added test cases

---
+++ Sort/test_sort.py
@@ -0,0 +1,34 @@
+import pytest
+from insert_bubble import insertion_sort, bubble_sort
+from merge_sort import merge_sort, _join, _tail_merge
+
+@pytest.fixture(scope="function")
+def build_list():
+    unsort = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    sort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return unsort, sort
+
+
+def test_insertion(build_list):
+    x, y = build_list
+    assert insertion_sort(x) == y
+
+
+def test_bubble(build_list):
+    x, y = build_list
+    assert bubble_sort(x) == y
+
+
+def test_merge(build_list):
+    x, y = build_list
+    assert merge_sort(x) == y
+
+
+def test_merge_join(build_list):
+    x, y = build_list
+    assert _join(x, y) == y + x
+
+
+def test_merge_tail(build_list):
+    x, y = build_list
+    assert _tail_merge(x, y) == insertion_sort(x + y)
