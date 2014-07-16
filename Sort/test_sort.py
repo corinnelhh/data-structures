ab7abc4a9301991ed7fde75b4bb8c85beba1998c
drytuna <drytuna@gmail.com>
2014-07-16 10:10:02 -0700

    finished quick_sort.py, need time recording

--- Sort/test_sort.py
+++ Sort/test_sort.py
@@ -1,6 +1,7 @@
 import pytest
 from insert_bubble import insertion_sort, bubble_sort
 from merge_sort import merge_sort, _join, _tail_merge
+from quick_sort import quick_sort, _median
 
 @pytest.fixture(scope="function")
 def build_list():
@@ -32,3 +33,19 @@ def test_merge_join(build_list):
 def test_merge_tail(build_list):
     x, y = build_list
     assert _tail_merge(x, y) == insertion_sort(x + y)
+
+
+def test_median():
+    assert _median(1, 2, 3) == 2
+
+
+def test_quick_sort(build_list):
+    x, y = build_list
+    assert quick_sort(x) == y
+
+    import random
+    for i in xrange(100):
+        x = [random.randint(10,100) for i in xrange(20)]
+        y = merge_sort(x)
+        z = quick_sort(x)
+        assert y == z
